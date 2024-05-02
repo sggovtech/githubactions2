cf9e11cd8560a8f9131945cb94673551493ee1f6
shantanu561993@gmail.com <shantanu561993@gmail.com>
2024-05-02 12:56:52 +0800

    N Block Deals Download

--- N_bulk_block_deals_downloader.py
+++ N_bulk_block_deals_downloader.py
@@ -23,7 +23,7 @@ async def download_NSE_bulk_block_data(start_date:datetime.datetime.date, end_da
     start_date = start_date.strftime('%d-%m-%Y')
     end_date = end_date.strftime('%d-%m-%Y')
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate, br", "Referer": "https://www.nseindia.com/report-detail/display-bulk-and-block-deals", "Upgrade-Insecure-Requests": "1", "Sec-Fetch-Dest": "document", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-Site": "same-origin", "Te": "trailers"}
-    url_init = "https://www.nseindia.com"
+    url_init = os.environ["NSE_MAIN"]
     response = await client.get(url_init, headers=headers)
     cookies = response.cookies.items()
     cookies = {name: value for name, value in cookies}
@@ -46,11 +46,24 @@ async def download_NSE_bulk_block_data(start_date:datetime.datetime.date, end_da
     concat_db.drop_duplicates(subset=['Date','Symbol','Name','Client_Name','Deal_Type','Quantity_Traded','Price','Remarks'],inplace=True)
     concat_db.to_csv('N_bulk_block_deals.csv',index=False)
 
-def submit_NSE_bulk_block_data():
+async def submit_NSE_bulk_block_data():
     """
     Submits the NSE bulk block data to the database.
     """
-    httpx.post
+    csv_file = 'N_bulk_block_deals.csv'
+    # Upload the CSV file
+    with open(csv_file, 'rb') as file:
+        url = os.environ['BLOCK_SUBMIT_URL']
+        headers = {'X-BEARER':os.environ["JWT_SECRET"]}
+        response = httpx.post(url, files={'file': file}, headers=headers)
+    
+    # Check the response status
+    if response.status_code == 200 and response.json().get('message') == 'Task Initiated':
+        print("CSV file uploaded successfully.")
+    else:
+        print(response.status_code)
+        print(response.text)
+        print("Failed to upload CSV file.")
 
 
 if __name__ == "__main__":
@@ -58,5 +71,7 @@ if __name__ == "__main__":
         start_date = datetime.datetime.strptime(sys.argv[1], '%d-%m-%Y').date()
         end_date = datetime.datetime.strptime(sys.argv[2], '%d-%m-%Y').date()
         asyncio.run(download_NSE_bulk_block_data(start_date, end_date))
+        asyncio.run(submit_NSE_bulk_block_data())
     elif len(sys.argv) == 1:
         asyncio.run(download_NSE_bulk_block_data(datetime.date.today(), datetime.date.today()))
+        asyncio.run(submit_NSE_bulk_block_data())
