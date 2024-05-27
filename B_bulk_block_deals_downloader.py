dbca4226963e0db3c42791618e95a4998f931692
shantanu561993@gmail.com <shantanu561993@gmail.com>
2024-05-27 21:27:53 +0800

    BSE BULK Fixes 2

--- B_bulk_block_deals_downloader.py
+++ B_bulk_block_deals_downloader.py
@@ -3,6 +3,11 @@ import datetime
 import requests
 import io
 from bs4 import BeautifulSoup
+import sys
+import asyncio
+import os
+import httpx
+from libs.myhttpxlib import make_request
 
 
 async def get_hidden_input(content):
@@ -23,7 +28,7 @@ async def get_hidden_input(content):
             continue
     return tags
 
-async def download_BSE_bulk_block_data(start_date:datetime.datetime.date, end_date:datetime.datetime.date) -> pd.DataFrame:
+async def download_BSE_bulk_block_data(start_date:datetime.datetime.date, end_date:datetime.datetime.date) -> int:
     """
     Requests BSE (Bombay Stock Exchange) data for a given date range.
     
@@ -35,7 +40,7 @@ async def download_BSE_bulk_block_data(start_date:datetime.datetime.date, end_da
         pd.Dataframe : Pandas Dataframe.
     """
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Content-Type": "application/x-www-form-urlencoded", "Origin": "https://www.bseindia.com", "Referer": "https://www.bseindia.com/markets/equity/eqreports/bulknblockdeals.aspx", "Upgrade-Insecure-Requests": "1", "Sec-Fetch-Dest": "document", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-Site": "same-origin", "Sec-Fetch-User": "?1", "Te": "trailers"}
-    url = f"https://www.bseindia.com/markets/equity/eqreports/bulknblockdeals.aspx"
+    url = os.environ['BSE_BULK_DEALS']
     response = requests.get(url, headers=headers)
     hidden = await get_hidden_input(response.content)
     values = {
@@ -79,5 +84,68 @@ async def download_BSE_bulk_block_data(start_date:datetime.datetime.date, end_da
     concat_db['Price'] = concat_db['Price'].astype(float)
     concat_db.drop_duplicates(subset=['Date','BSE_Code','Symbol','Client_Name','Deal_Type','Quantity_Traded','Price'],inplace=True)
     concat_db["Remarks"] = "-"
-    return concat_db
+    concat_db.dropna(inplace=True)
+    concat_db.to_csv('B_bulk_block_deals.csv',index=False)
+    return len(concat_db)
 
+async def submit_BSE_bulk_block_data():
+    """
+    Submits the NSE bulk block data to the database.
+    """
+    csv_file = 'B_bulk_block_deals.csv'
+    # Upload the CSV file
+    
+    with open(csv_file, 'rb') as file:
+        url = os.environ['BLOCK_SUBMIT_URL']
+        headers = {'X-BEARER':os.environ["JWT_SECRET"]}
+        attempts = 5
+        while attempts > 0:
+            attempts -= 1
+            try:
+                response = httpx.post(url, files={'file': file}, headers=headers)
+                break
+            except Exception as e:
+                print(f"Error: {e} Retrying...")
+                continue
+        
+    # Check the response status
+    if response.status_code == 200 and response.json().get('message') == 'Task Initiated':
+        print("CSV file uploaded successfully.")
+    else:
+        print(response.status_code)
+        print(response.text)
+        print("Failed to upload CSV file.")
+
+async def get_latest_dates():
+    """
+    Fetches the latest date from the database.
+    """
+    async with httpx.AsyncClient() as client:
+        response = await make_request(client=client,url="https://backend.tradeaajka.com/stock_data/search_block_deals",json_data={"skip": 0,"limit": 1},method="POST",headers={"X-BEARER":os.environ["JWT_SECRET"]},limit=100)
+        if response.status_code == 200 and response.json().get('count')==0: # case when no data is present in the database
+            end_date = datetime.date.today()
+            start_date = end_date - datetime.timedelta(days=30)
+            return start_date, end_date
+        elif response.status_code == 200 and response.json().get('count')>0:
+            deals = response.json().get('deals')
+            last_date = datetime.datetime.strptime(deals[0].get('date'), '%d-%b-%y').date()
+            start_date = last_date - datetime.timedelta(days=3)
+            end_date = datetime.date.today()
+            return start_date, end_date    
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        start_date = datetime.datetime.strptime(sys.argv[1], '%d-%m-%Y').date()
+        end_date = datetime.datetime.strptime(sys.argv[2], '%d-%m-%Y').date()
+        len_db = asyncio.run(download_BSE_bulk_block_data(start_date, end_date))
+        if len_db>0:
+            asyncio.run(submit_BSE_bulk_block_data())
+        else:
+            print("No data found for the given date range.")
+    elif len(sys.argv) == 1:
+        start_date,end_date = asyncio.run(get_latest_dates())
+        len_db = asyncio.run(download_BSE_bulk_block_data(start_date=start_date, end_date=end_date))
+        if len_db > 0:
+            asyncio.run(submit_BSE_bulk_block_data())
+        else:
+            print("No data found for the given date range.")
